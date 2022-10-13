006a5c1a470d786466eaa7a56bbeb0be2e068d3e
KuroiOni13 <danilacool7@gmail.com>
2022-10-13 16:53:21 +0600

    Newbox2

--- src/org/program1/treningjavaclass/Program1.java
+++ src/org/program1/treningjavaclass/Program1.java
@@ -3,8 +3,6 @@ package org.program1.treningjavaclass;
 public class Program1 {
     public static void main(String[] args) {
         System.out.println("WorkingTest");
-<<<<<<< HEAD
-=======
         Box mybox = new Box();
         double vol;
         mybox.height = 10;
@@ -13,7 +11,5 @@ public class Program1 {
         vol = mybox.depth * mybox.height * mybox.width;
         System.out.println( "Vol is " + vol );
 
-
->>>>>>> c2123b1 (Newbox)
     }
 }
